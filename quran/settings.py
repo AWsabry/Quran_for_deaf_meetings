10f9598f479677f7565b6dee0a8ddb583a2691c8
= <=>
2022-07-22 22:21:00 +0200

    Add Static Root

--- quran/settings.py
+++ quran/settings.py
@@ -124,6 +124,7 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_DIR = BASE_DIR / 'static'
 STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
