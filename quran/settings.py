ee02bbb7ec36b4f7f26c972433879fe8db8910b2
= <=>
2022-08-01 23:42:35 +0200

    Add Messages

--- quran/settings.py
+++ quran/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,14 @@ AGORA_APP_ID = "5ed46348b4204814a058a1171c7e3f18"
 AGORA_APP_CERTIFICATE = 'dcccf6678a04408e9ddcbac9ebdb7b5e'
 AGORA_EXPIRATION_TIME = 3600 * 24  # in seconds (s * m * h)
 AGORA_INCREASE_TIME = 60 * 30  # in seconds (s * m * h)
+
+
+# Messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+MESSAGE_LEVEL = messages.DEBUG
